72f9e2d492eb9956f377c4087df3304ea03a7a33
wendydv1989 <wendydv1989@gmail.com>
2020-11-06 23:50:37 +0100

    works

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,28 @@ const posthtml = require('gulp-posthtml');
 const posthtmlInlineAssets = require('posthtml-inline-assets');
 const inlineFonts = require('gulp-inline-fonts');
 
-let inline_fonts = function () {
-  return src(['src/assets/fonts/*.woff'])
-    .pipe(inlineFonts({ name: 'WorkSansRegular' }))
-    .pipe(dest('src'));
+let inline_fonts1 = function () {
+  return src(['src/assets/fonts/IBMPlexMonoRegular.woff'])
+    .pipe(inlineFonts({ name: 'IBMPlexMonoRegular' }))
+    .pipe(dest('generated-fonts'));
+};
+
+let inline_fonts2 = function () {
+  return src(['src/assets/fonts/IBMPlexMono500.woff'])
+    .pipe(inlineFonts({ name: 'IBMPlexMono500' }))
+    .pipe(dest('generated-fonts'));
+};
+
+let inline_fonts3 = function () {
+  return src(['src/assets/fonts/IBMPlexMono600.woff'])
+    .pipe(inlineFonts({ name: 'IBMPlexMono600' }))
+    .pipe(dest('generated-fonts'));
+};
+
+let inline_fonts4 = function () {
+  return src(['src/assets/fonts/WorkSans500.woff'])
+    .pipe(inlineFonts({ name: 'WorkSans500' }))
+    .pipe(dest('generated-fonts'));
 };
 
 let inline_js_css = function () {
@@ -22,6 +40,9 @@ let inline_js_css = function () {
 
 // exports.inline_js_css = inline_js_css;
 exports.default = series(
-  inline_fonts,
+  inline_fonts1,
+  inline_fonts2,
+  inline_fonts3,
+  inline_fonts4,
   inline_js_css
 );
