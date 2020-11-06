41760e13cd18f8ba1055026eae65b067ec3dfa3d
wendydv1989 <wendydv1989@gmail.com>
2020-11-06 22:12:43 +0100

    gulping the night away

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,23 @@
 const { src, dest, task, series } = require('gulp');
-
 const posthtml = require('gulp-posthtml');
 const posthtmlInlineAssets = require('posthtml-inline-assets');
-
 const inlineFonts = require('gulp-inline-fonts');
 
-exports.default = function() {
-  return src('dist/*.html')
-  .pipe(
-  posthtml([
-    posthtmlInlineAssets({
-    	cwd: 'dist'
-    })
-  ])
-  ).pipe(dest('dist-html-only/'));
-}
-
-let inline_fonts = function() {
-return src(['dist/fonts/*.woff'])
-  .pipe(inlineFonts({ name: 'DOS' }))
-  .pipe(dest('dist/'));
+let inline_fonts = function () {
+  return src(['src/assets/fonts/*.woff'])
+    .pipe(inlineFonts({ name: 'WorkSansRegular' }))
+    .pipe(dest('src'));
 };
 
-let inline_js_css = function() {
-  return src('dist/*.html')
-  .pipe(
-  posthtml([
-    posthtmlInlineAssets({
-    	cwd: 'dist'
-    })
-  ])
-  ).pipe(dest('dist-html-only/'));
+let inline_js_css = function () {
+  return src('src/*.html')
+    .pipe(
+      posthtml([
+        posthtmlInlineAssets({
+          cwd: 'src'
+        })
+      ])
+    ).pipe(dest('dist/'));
 };
 
 // exports.inline_js_css = inline_js_css;
@@ -38,18 +25,3 @@ exports.default = series(
   inline_fonts,
   inline_js_css
 );
-
-// exports.default = function() {
-//   return src('dist/*.html')
-//   .pipe(
-//   posthtml([
-//     posthtmlInlineAssets({
-//     	cwd: '/Users/sig/Code/swarm2/swapchat/dist'
-//     })
-//   ])
-//   ).pipe(dest('output2/'));
-// }
-
-// .pipe(inlineFonts({ name: 'icons' }))
-
-// exports.default =
